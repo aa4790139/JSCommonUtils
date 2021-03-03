25016ef9d26748faa976fa3f03190a1a19e44917
Potter <75838259@qq.com>
2021-03-03 13:23:14 +0800

    @Potter：添加相关注释

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -9,10 +9,17 @@ import {terser} from "rollup-plugin-terser";
 export default {
     input: 'src/*.js',
     output: {
+        /*输出公共库路径*/
         file: 'lib/index.js',
+        /*配置：UMD 通用定义模块，支持前端端跨平台模块化*/
         format: 'umd',
         name: 'JSCommonUtils',
-        plugins: [terser(), uglify()]
+        /*压缩和混淆js*/
+        plugins: [terser(), uglify()],
+        /*JS前缀: 添加库相关信息*/
+        banner: '/* JSCommonUtils version ' + version + ' */',
+        /*JS尾部: 添加个人相关信息*/
+        footer: '/* follow me on github aa4790139 */'
     },
     plugins: [
         json(),
@@ -20,8 +27,7 @@ export default {
             /*过滤node_modules编译*/
             exclude: 'node_modules/**'
         }),
+        /*允许多输入源*/
         multiEntry()
     ],
-    banner: '/* JSCommonUtils version ' + version + ' */',
-    footer: '/* follow me on github aa4790139 */'
 };
